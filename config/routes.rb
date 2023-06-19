8c316075c2e0c3c1d8699df66178b0dac4169a90
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-06-19 09:24:18 +0100

    amended route for index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'companies/index'
+  # get 'companies/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  resources :users, only: :index do
-    resources :companies, only: [:index]
-  end
+  resources :companies, only: [:index]
 end
