2ae211a499139b71f29e03769e1bd5c2de49a32c
sam arman <97samarman@gmail.com>
2023-06-19 22:17:19 +0100

    added routes actions and views for new, edit, delete etc

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :companies, only: [:index]
+  resources :companies, only: [:index, :create, :new, :destroy, :show, :edit, :update]
 end
