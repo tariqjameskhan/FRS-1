9eb808ce01c06fa2ef999c11ed1db198da7b6b01
sam arman <97samarman@gmail.com>
2023-06-21 23:16:45 +0100

    check the new migration file and the schema to make sure i did it right

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_202331) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_21_221524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,5 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_202331) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "companies", "users", column: "client_id"
   add_foreign_key "companies", "users", column: "inspector_id"
 end
