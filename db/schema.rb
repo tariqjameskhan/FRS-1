ad8907a18e280a8b39996fe9a66bc695f985bea4
sam arman <97samarman@gmail.com>
2023-06-25 18:54:19 +0100

    fixed int issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_06_25_174503) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "telephone_number"
+    t.string "telephone_number"
     t.string "email_address"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
