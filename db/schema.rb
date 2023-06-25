ea89f21e226f6eeaeaed6dbd6c59ad8a5f1cf1fc
ashish dhawan <ashish.dhawan2@gmail.com>
2023-06-25 16:01:54 +0100

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_221524) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_173536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.string "telephone_number"
+    t.integer "telephone_number"
     t.string "email_address"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "inspector_id"
     t.bigint "client_id"
     t.index ["client_id"], name: "index_companies_on_client_id"
     t.index ["inspector_id"], name: "index_companies_on_inspector_id"
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.string "premises_name"
+    t.string "street_address"
+    t.string "telephone_number"
+    t.date "date_report_completed"
+    t.string "city"
+    t.string "postcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_221524) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "companies", "users"
   add_foreign_key "companies", "users", column: "client_id"
   add_foreign_key "companies", "users", column: "inspector_id"
 end
