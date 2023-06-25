6bbdf570e7c6fc2ed0b5a3f578c1a655371ffc67
ashish dhawan <ashish.dhawan2@gmail.com>
2023-06-25 17:14:11 +0100

    added associations and validtions to report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,8 @@
 class Report < ApplicationRecord
+  has_many :questions, class_name: 'Question', foreign_key: :report_id
+  belongs_to :companies, class_name: 'Company', foreign_key: :company_id
+  belongs_to :user, class_name: 'User', foreign_key: :client_id
+
+  validates :company_id, uniqueness: true
+  validates :client_id, uniqueness: true
 end
