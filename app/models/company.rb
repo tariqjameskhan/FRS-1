c2c6bdc681065ec4be2a2a76cf8601613dfde1f5
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-06-26 09:33:20 +0100

    added report model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ApplicationRecord
   belongs_to :inspector, class_name: 'User', :foreign_key => :inspector_id
   belongs_to :client, class_name: 'User', :foreign_key => :client_id
+  has_many :reports
 end
