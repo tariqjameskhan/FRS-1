75c09d65807559c3571ff9ff7d0746b18638a47e
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-06-26 09:40:49 +0100

    amended route as going to be on comp show page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :companies do
-    resources :reports, only: [:index, :show]
+    resources :reports, only: [:show]
   end
 end
