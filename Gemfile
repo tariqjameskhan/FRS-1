8fd37093bfd6b5b7adc727ee4f065cd26bb6eb70
Denis Akan <denis.akan@gmail.com>
2023-06-28 20:17:10 +0200

    added map to companies show page

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem "bootsnap", require: false
 # Use Sass to process CSS
 gem "sassc-rails"
 
+gem "geocoder"
+
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 gem 'pundit'
@@ -76,4 +79,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_082746) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_28_090406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_082746) do
     t.datetime "updated_at", null: false
     t.bigint "inspector_id"
     t.bigint "client_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["client_id"], name: "index_companies_on_client_id"
     t.index ["inspector_id"], name: "index_companies_on_inspector_id"
   end
