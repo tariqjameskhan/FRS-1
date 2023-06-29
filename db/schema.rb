bada88f0aae1072d94568af2a922c37061068a0d
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-06-29 15:24:43 +0100

    removedcolumns from question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_29_131508) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_29_142409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,11 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_131508) do
 
   create_table "questions", force: :cascade do |t|
     t.string "question_name"
-    t.boolean "answer"
-    t.text "fault"
-    t.text "recommendation"
-    t.string "priority"
-    t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
