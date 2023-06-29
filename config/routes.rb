d2e4a3811395d945356ce987a5c0bd243772fcd3
Denis Akan <denis.akan@gmail.com>
2023-06-29 11:56:46 +0200

    commented out 'home' from navbar and redirect to companies after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'companies/index'
   devise_for :users
-  root to: "pages#home"
+  root to: "companies#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
