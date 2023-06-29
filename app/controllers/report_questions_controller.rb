1e2fb5b8e57ef8bb72f6e01b4db3ef8214eb657c
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-06-29 21:13:25 +0100

    made it so edit report questions actually works

--- app/controllers/report_questions_controller.rb
+++ app/controllers/report_questions_controller.rb
@@ -1,9 +1,24 @@
 class ReportQuestionsController < ApplicationController
-  def new
-    @report_question = ReportQuestion.new
+  def edit
+    @report_question = ReportQuestion.find(params[:id])
+    authorize @report_question
   end
 
-  def create
+  def update
+    @report_question = ReportQuestion.find(params[:id])
+    @company = Company.find(params[:company_id])
+    @report = Report.find(params[:report_id])
+    authorize @report_question
+    if @report_question.update(report_question_params)
+      redirect_to company_report_path(@company, @report)
+    else
+      render :edit
+    end
+  end
 
+  private
+
+  def report_question_params
+    params.require(:report_question).permit(:answer, :fault, :recommendation, :priority, :date)
   end
 end
