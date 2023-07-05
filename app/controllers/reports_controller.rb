2fdabb578023252aabf8b03601ba92f4c0a5a9cd
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-07-05 18:17:49 +0100

    added report params to include report questions

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -68,18 +68,22 @@ class ReportsController < ApplicationController
 
   private
 
+  # def report_params
+  #   params.require(:report).permit(
+  #     :premises_name,
+  #     :street_address,
+  #     :city, :postcode,
+  #     :telephone_number,
+  #     :date_report_completed,
+  #     :construction_details,
+  #     :number_of_floors,
+  #     :floor_area,
+  #     photos: []
+  #   )
+  # end
+
   def report_params
-    params.require(:report).permit(
-      :premises_name,
-      :street_address,
-      :city, :postcode,
-      :telephone_number,
-      :date_report_completed,
-      :construction_details,
-      :number_of_floors,
-      :floor_area,
-      photos: []
-    )
+    params.require(:report).permit(:premises_name, :street_address, :city, :postcode, :telephone_number, :date_report_completed, photos: [], report_questions_attributes: [:id, :answer, :fault, :recommendation, :date, :priority, :question_id])
   end
 
   def set_company
