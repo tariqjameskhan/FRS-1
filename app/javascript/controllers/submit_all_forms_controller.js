60533a880918ce5ee259be90f2d153928f3b4dcf
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-07-05 18:29:26 +0100

    added nested forms again but working this time

--- app/javascript/controllers/submit_all_forms_controller.js
+++ app/javascript/controllers/submit_all_forms_controller.js
@@ -11,23 +11,23 @@ export default class extends Controller {
     // this.csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content")
   }
 
-  send(event) {
-    event.preventDefault()
-    console.log(event)
-    const url = this.formTarget.action
-    // console.log(this.formTarget.action)
-    const options = {
-      method: "PATCH",
-      // could be POST
-      headers: { "Accept": "application/json", "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').getAttribute("content")},
-      body: new FormData(this.formTarget)
-    }
-    fetch(url, options)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data)
-    })
-  }
+  // send(event) {
+  //   event.preventDefault()
+  //   console.log(event)
+  //   const url = this.formTarget.action
+  //   // console.log(this.formTarget.action)
+  //   const options = {
+  //     method: "PATCH",
+  //     // could be POST
+  //     headers: { "Accept": "application/json", "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').getAttribute("content")},
+  //     body: new FormData(this.formTarget)
+  //   }
+  //   fetch(url, options)
+  //   .then(response => response.json())
+  //   .then((data) => {
+  //     console.log(data)
+  //   })
+  // }
 
   click(event) {
     event.preventDefault()
